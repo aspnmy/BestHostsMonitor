8b53199eafc48f6b4a76ca959a918f04fe5882fb
aspnmy <ghjhllning@gmail.com>
2025-01-15 06:12:04 +0800

    Auto Fix commit

--- bin/client/getBestHostsClient.sh
+++ bin/client/getBestHostsClient.sh
@@ -6,15 +6,15 @@ CURRENT_DIR=$(cd "$(dirname "$0")" || exit; pwd) # 当前脚本所在目录
 PARENT_DIR=$(dirname "$CURRENT_DIR") # 当前脚本所在目录的上级目录
 # 公共入口
 wURL=""
-# Cn区
-Cn_hosts="${wURL}/cn/hosts.list"
-Cn_sources="${wURL}/cn/sources.list"
-Cn_docker="${wURL}/cn/docker.list"
+# # Cn区
+# Cn_hosts="${wURL}/CN/hosts.list"
+# Cn_sources="${wURL}/CN/sources.list"
+# Cn_docker="${wURL}/CN/docker.list"
 
-# En区
-En_hosts="${wURL}/en/hosts.list"
-En_sources="${wURL}/en/sources.list"
-En_docker="${wURL}/en/docker.list"
+# # En区
+# En_hosts="${wURL}/EN/hosts.list"
+# En_sources="${wURL}/EN/sources.list"
+# En_docker="${wURL}/EN/docker.list"
 
 CF_BESTIP="${CURRENT_DIR}/cfbestip_hosts.txt"
 get_latest_CF_BESTIP() {
@@ -41,7 +41,9 @@ ck_Country(){
 	local myIPCountry
 	myIPCountry=$(get_myIPCountry)
     if [ "$myIPCountry" = "中国" ]; then
-   		cfhosts_files="cfhosts-cn.aspnmy"
+    hosts="${wURL}/CN/hosts.list"
+    sources="${wURL}/CN/sources.list"
+    Cn_docker="${wURL}/CN/docker.list"
         
     else
         cfhosts_files="cfhosts-en.aspnmy"
